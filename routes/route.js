c4f632f9605e39ece01651ebdf1d42564dca192f
Vamsi Ammineni <vams4u.499@gmail.com>
2018-01-21 17:09:12 +0100

    jsonwebtoken added

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,16 @@
 const express = require('express');
+var app = express();
 const router = express.Router();
-
+var jwt    = require('jsonwebtoken'); 
+var bcrypt = require('bcrypt');
+app.set('superSecret', 'ilovescotchyscotch'); // secret variable
 const user = require('../models/users');
 
+router.get('/', function(req, res){
+res.send('api home here');
+});
+
+// register an user
 router.post('/register', function(req, res){
 	if(req.body.first_name && req.body.last_name && req.body.email_id && req.body.password) {
 		var newuser = new user({
@@ -12,7 +20,7 @@ router.post('/register', function(req, res){
 			password: req.body.password
 			});
 		}
-		
+
 	newuser.save(function (err) {
     if (err) {
       res.send('err');
@@ -23,8 +31,9 @@ router.post('/register', function(req, res){
 
 });
 
+// get the registered users
 router.get('/users', function(req, res){
-users.find(function(err, users){
+user.find(function(err, users){
 if (err){
 	res.json(err);
 }
@@ -32,6 +41,44 @@ else {
 	res.json(users);
 }
 });
+});
+
+// logon 
+router.post('/logon', function(req, res){
+user.findOne({email_id: req.body.email_id}, function(err, user){
+	if(err) throw err;
+	if(!user) {
+		res.json({success : false, message: 'Authentication failed, User not found'});
+	}
+	else if(user){
+        bcrypt.compare(req.body.password, user.password, function (err, result) {
+          if(err){
+          	res.json('password doesnot match, Authentication failed');
+          }
+          else {
+
+        // if user is found and password is right
+        // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+      email_id: user.email_id 
+    };
+    console.log(payload);
+        var token = jwt.sign(payload, app.get('superSecret'), {
+          expiresIn: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+        });
+	}
+})
 });
 
 module.exports = router;
